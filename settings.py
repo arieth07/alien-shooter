0d47f8c0ddcad6076922ac2668a3f43aafc51edf
arian <arrustemi@student.ethz.ch>
2024-05-03 11:07:49 +0200

    speed in x- direction is now adjustable

--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (255, 255, 255)
+
+        # Ship settings
+        self.ship_speed = 1.5 # Adjust the ship's speed 1.5 pixel per iteration
