f6fa7729802273834e6f9fb21cc1c488c106ad26
arian <arrustemi@student.ethz.ch>
2024-05-04 18:06:37 +0200

    mouse cursor disappears while Playing the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -191,6 +191,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
@@ -215,6 +216,9 @@ class AlienInvasion:
             # Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
